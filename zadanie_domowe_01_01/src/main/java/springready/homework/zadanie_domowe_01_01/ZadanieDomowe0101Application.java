ce34a30962ca5fd9d1a6f3e2b36424cf4cd4a50d
Maksim29AM <maks.abramovich.1995@mail.ru>
2025-09-09 21:30:43 +0200

    Initial commit

---
+++ zadanie_domowe_01_01/src/main/java/springready/homework/zadanie_domowe_01_01/ZadanieDomowe0101Application.java
@@ -0,0 +1,13 @@
+package springready.homework.zadanie_domowe_01_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieDomowe0101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadanieDomowe0101Application.class, args);
+    }
+
+}
---
+++ zadanie_domowe_01_01/src/test/java/springready/homework/zadanie_domowe_01_01/ZadanieDomowe0101ApplicationTests.java
@@ -0,0 +1,13 @@
+package springready.homework.zadanie_domowe_01_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadanieDomowe0101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
