54e0ddb6dc7f8c0e2770635106d04fc48baf6531
Maksim29AM <maks.abramovich.1995@mail.ru>
2025-09-09 22:57:53 +0200

    Add QuoteProxy component for fetching and mapping quotes

---
+++ zadanie_domowe_01_01/src/main/java/springready/homework/zadanie_domowe_01_01/proxy/quote/QuoteProxy.java
@@ -0,0 +1,70 @@
+package springready.homework.zadanie_domowe_01_01.proxy.quote;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+@Log4j2
+public class QuoteProxy {
+
+
+    private final RestTemplate restTemplate;
+
+    public QuoteProxy(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Value("${quote-server.service.url}")
+    String url;
+
+    @Value("${quote-server.service.port}")
+    String port;
+
+    private String makeRequest() {
+//        String uri = "http://localhost:8080/api/random";
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                .newInstance()
+                .scheme("http")
+                .host(url)
+                .port(port)
+                .path("/api/random");
+        try {
+            ResponseEntity<String> exchange = restTemplate.exchange(
+                    uriBuilder.build().toUri(),
+                    HttpMethod.GET,
+                    null,
+                    String.class
+            );
+            return exchange.getBody();
+        } catch (RestClientResponseException exception) {
+            log.info("Bad request: " + exception.getStatusText() + " " + exception.getStatusCode().value());
+        } catch (RestClientException exception) {
+            log.info(exception.getMessage());
+        }
+        return null;
+    }
+
+    public void fetchQuote() throws JsonProcessingException {
+        String json = makeRequest();
+        if (json != null) {
+            Quote quote = mapJsonToQuoteResponse(json);
+            log.info(quote.value());
+        }
+    }
+
+    private static Quote mapJsonToQuoteResponse(String json) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, Quote.class);
+
+    }
+
+}
