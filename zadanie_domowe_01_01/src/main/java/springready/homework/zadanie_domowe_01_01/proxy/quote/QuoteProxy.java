3e3b67d3af8f6c892e924f20d6f3aa89f8b3386b
Maksim29AM <maks.abramovich.1995@mail.ru>
2025-09-11 21:18:10 +0200

    Extend `QuoteProxy` to support posting quotes via POST request

--- zadanie_domowe_01_01/src/main/java/springready/homework/zadanie_domowe_01_01/proxy/quote/QuoteProxy.java
+++ zadanie_domowe_01_01/src/main/java/springready/homework/zadanie_domowe_01_01/proxy/quote/QuoteProxy.java
@@ -2,6 +2,7 @@ package springready.homework.zadanie_domowe_01_01.proxy.quote;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -49,12 +50,36 @@ public class QuoteProxy {
         return makeGetRequestQuote(uriBuilder);
     }
 
+    public String postQuote(String quote) {
+        URI uriBuilder = buildOneQuoteUri();
+        return makePostRequestQuote(uriBuilder,quote);
+    }
+
     private String makeGetRequestQuote(URI uri) {
         try {
             ResponseEntity<String> exchange = restTemplate.exchange(
                     uri,
                     HttpMethod.GET,
                     null,
+                    String.class
+            );
+            return exchange.getBody();
+        } catch (RestClientResponseException exception) {
+            log.info("Bad request: " + exception.getStatusText() + " " + exception.getStatusCode().value());
+        } catch (RestClientException exception) {
+            log.info(exception.getMessage());
+        }
+        return null;
+    }
+
+    private String makePostRequestQuote(URI uri, String quote) {
+        QuoteRequest quoteRequest = new QuoteRequest(quote);
+        HttpEntity<QuoteRequest> objectHttpEntity = new HttpEntity<>(quoteRequest);
+        try {
+            ResponseEntity<String> exchange = restTemplate.exchange(
+                    uri,
+                    HttpMethod.POST,
+                    objectHttpEntity,
                     String.class
             );
             return exchange.getBody();
@@ -102,5 +127,15 @@ public class QuoteProxy {
                 .port(port)
                 .path("/apiWithRequestParam")
                 .queryParam("id", id).build().toUri();
+    }
+
+    private URI buildOneQuoteUri() {
+        return UriComponentsBuilder
+                .newInstance()
+                .scheme("http")
+                .host(url)
+                .port(port)
+                .path("/api/quote")
+                .build().toUri();
     }
 }
