56e72a19021fee2b2b4196f7b20733a135755729
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-09-15 18:53:51 +0200

    Initial commit

---
+++ zadanie_domowe_02_02/src/main/java/com/zadanie_domowe/ZadanieDomowe0202Application.java
@@ -0,0 +1,13 @@
+package com.zadanie_domowe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieDomowe0202Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadanieDomowe0202Application.class, args);
+    }
+
+}
