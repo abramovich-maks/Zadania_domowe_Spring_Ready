9e348bcf5a5f902247416d623bf5067b569a90f3
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-09-18 17:40:38 +0200

    Add `MainApplicationRunner` to log GitHub user repositories

---
+++ zadanie_domowe_02_02vRestT/src/main/java/com/zadanie_domowe/MainApplicationRunner.java
@@ -0,0 +1,31 @@
+package com.zadanie_domowe;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.zadanie_domowe.github.GithubResult;
+import com.zadanie_domowe.github.GithubService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Log4j2
+public class MainApplicationRunner {
+
+    private final GithubService githubService;
+
+    public MainApplicationRunner(GithubService githubService) {
+        this.githubService = githubService;
+    }
+
+
+    public void start(String userName) throws JsonProcessingException {
+        List<GithubResult> repos = githubService.fetchUserRepos(userName);
+        for (GithubResult repo : repos) {
+            log.info("Repo: {}, Owner: {}, Fork: {}",
+                    repo.name(),
+                    repo.owner().login(),
+                    repo.fork());
+        }
+    }
+}
