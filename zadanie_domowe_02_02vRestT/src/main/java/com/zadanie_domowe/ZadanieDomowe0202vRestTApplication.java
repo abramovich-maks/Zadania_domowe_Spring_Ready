a20691bdb1baf4e706ab7b7a7a75f9f6687563f5
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-09-18 15:33:58 +0200

    Initial commit

---
+++ zadanie_domowe_02_02vRestT/src/main/java/com/zadanie_domowe/ZadanieDomowe0202vRestTApplication.java
@@ -0,0 +1,13 @@
+package com.zadanie_domowe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieDomowe0202vRestTApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadanieDomowe0202vRestTApplication.class, args);
+    }
+
+}
