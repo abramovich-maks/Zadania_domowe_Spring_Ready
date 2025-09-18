eae0d33b7d1ba4c2bfb5152cc1fde4b83e5983b2
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-09-18 17:39:48 +0200

    Run `MainApplicationRunner` on application startup

--- zadanie_domowe_02_02vRestT/src/main/java/com/zadanie_domowe/ZadanieDomowe0202vRestTApplication.java
+++ zadanie_domowe_02_02vRestT/src/main/java/com/zadanie_domowe/ZadanieDomowe0202vRestTApplication.java
@@ -1,13 +1,26 @@
 package com.zadanie_domowe;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class ZadanieDomowe0202vRestTApplication {
+
+    private final MainApplicationRunner runner;
+
+    public ZadanieDomowe0202vRestTApplication(MainApplicationRunner runner) {
+        this.runner = runner;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ZadanieDomowe0202vRestTApplication.class, args);
     }
 
+    @EventListener(ApplicationStartedEvent.class)
+    public void run() throws JsonProcessingException {
+        runner.start("abramovich-maks");
+    }
 }
