fcf768b75eaadc8527438a8218e975808ea5b690
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-09-19 14:56:15 +0200

    Refactor `start` method to fetch and log repositories with branches

--- zadanie_domowe_02_02vRestT/src/main/java/com/zadanie_domowe/ZadanieDomowe0202vRestTApplication.java
+++ zadanie_domowe_02_02vRestT/src/main/java/com/zadanie_domowe/ZadanieDomowe0202vRestTApplication.java
@@ -22,7 +22,5 @@ public class ZadanieDomowe0202vRestTApplication {
     @EventListener(ApplicationStartedEvent.class)
     public void run() throws JsonProcessingException {
         runner.start("abramovich-maks");
-
-        runner.startBranchRepo("abramovich-maks","SproutSync");
     }
 }
