5c1e4ccfcf975df5d393897bc5a5720613d25041
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-09-19 10:41:28 +0200

    Add method to fetch branches

--- zadanie_domowe_02_02vRestT/src/main/java/com/zadanie_domowe/github/service/GithubService.java
+++ zadanie_domowe_02_02vRestT/src/main/java/com/zadanie_domowe/github/service/GithubService.java
@@ -3,11 +3,15 @@ package com.zadanie_domowe.github.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zadanie_domowe.github.proxy.BranchesResponse;
 import com.zadanie_domowe.github.proxy.GithubProxy;
 import com.zadanie_domowe.github.proxy.GithubResult;
+import com.zadanie_domowe.github.proxy.ResultResponseBranch;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -26,7 +30,7 @@ public class GithubService {
     public List<GithubResult> fetchUserRepos(String userName) throws JsonProcessingException {
         String json = githubClient.makeUserRepo(userName);
         if (json == null) {
-            log.error("User not found");
+            log.error("json was null ");
             return Collections.emptyList();
         }
 
@@ -38,4 +42,15 @@ public class GithubService {
         return response;
     }
 
+    public ResultResponseBranch fetchBranchRepos(String userName, String repoName) throws JsonProcessingException {
+        String json = githubClient.makeReposBranches(userName, repoName);
+        if (json == null) {
+            log.error("json was null ");
+            return new ResultResponseBranch(new ArrayList<>());
+        }
+        BranchesResponse[] response = objectMapper.readValue(
+                json, BranchesResponse[].class);
+        List<BranchesResponse> list = Arrays.asList(response);
+        return new ResultResponseBranch(new ArrayList<>(list));
+    }
 }
