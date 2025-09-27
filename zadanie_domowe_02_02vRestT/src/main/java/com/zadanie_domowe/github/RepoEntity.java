746a98c78f4b1faf295b89892acda527ccacbcaa
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-09-27 11:02:08 +0200

    Add RepoEntity class for mapping repository data in the database

---
+++ zadanie_domowe_02_02vRestT/src/main/java/com/zadanie_domowe/github/RepoEntity.java
@@ -0,0 +1,39 @@
+package com.zadanie_domowe.github;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "repo")
+@Getter
+@Setter
+@Builder
+public class RepoEntity {
+
+    public RepoEntity() {
+    }
+
+    public RepoEntity(String owner, String name) {
+        this.owner = owner;
+        this.name = name;
+    }
+
+    public RepoEntity(Long id, String owner, String name) {
+        this.id = id;
+        this.owner = owner;
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    private String owner;
+
+    private String name;
+
+
+}
